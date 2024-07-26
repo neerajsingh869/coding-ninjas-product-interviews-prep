e0819ad1337cec67f16863a769c97a10e0934f3f
Neeraj Singh <869neerajsingh@gmail.com>
2024-07-26 10:49:58 +0530

    feat: add another way to solve valid palindrome problem

--- twoPointers/ValidPalindrome2.java
+++ twoPointers/ValidPalindrome2.java
@@ -28,7 +28,7 @@ public class ValidPalindrome2 {
 		s consists of lowercase English letters.
 	 */
 	// Time complexity -> O(n), Space complexity -> O(1)
-	public boolean validPalindrome(String s) {
+	public boolean validPalindrome1(String s) {
         int n = s.length();
         int start = 0;
         int end = n - 1;
@@ -69,6 +69,38 @@ public class ValidPalindrome2 {
             }
         }
 
+        return true;
+    }
+	
+	// Optimized version
+	// Time complexity -> O(n), Space complexity -> O(1)
+	public boolean validPalindrome2(String s) {
+        int n = s.length();
+        int start = 0;
+        int end = n - 1;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return (isPalindrome(s, start + 1, end) || 
+                        isPalindrome(s, start, end - 1));
+            }
+
+            start++;
+            end--;   
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        
         return true;
     }
 
