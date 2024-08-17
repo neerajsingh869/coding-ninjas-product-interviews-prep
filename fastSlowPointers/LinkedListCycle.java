247e513cdd0784f64e24adbefac8549cc2e5767b
Neeraj Singh <869neerajsingh@gmail.com>
2024-08-17 15:58:42 +0530

    feat: find node where cycle began in linked list

--- fastSlowPointers/LinkedListCycle.java
+++ fastSlowPointers/LinkedListCycle.java
@@ -8,6 +8,21 @@ import GenericClasses.LinkedListNode;
 import GenericClasses.PrintList;
 
 public class LinkedListCycle {
+	/*
+	 * Given head, the head of a linked list, determine if the 
+	 * linked list has a cycle in it.
+
+		There is a cycle in a linked list if there is some node in 
+		the list that can be reached again by continuously following 
+		the next pointer. Internally, pos is used to denote the index 
+		of the node that tail's next pointer is connected to. Note that 
+		pos is not passed as a parameter.
+		
+		Return true if there is a cycle in the linked list. Otherwise, 
+		return false.
+	 */
+	
+	
 	// Time complexity -> O(n), Space complexity -> O(n)
 	public static boolean detectCycle1(LinkedListNode head) {
 	    Set<LinkedListNode> visited = new HashSet<>();
@@ -38,6 +53,72 @@ public class LinkedListCycle {
 
 	    return false;
 	  }
+	
+	/*
+	 * Given the head of a linked list, return the node where 
+	 * the cycle begins. If there is no cycle, return null.
+
+		There is a cycle in a linked list if there is some node in the 
+		list that can be reached again by continuously following the 
+		next pointer. Internally, pos is used to denote the index of 
+		the node that tail's next pointer is connected to (0-indexed). 
+		It is -1 if there is no cycle. Note that pos is not passed as 
+		a parameter.
+		
+		Do not modify the linked list.
+		
+		Example 1:
+		Input: head = [3,2,0,-4], pos = 1
+		Output: tail connects to node index 1
+		Explanation: There is a cycle in the linked list, 
+		where tail connects to the second node.
+		
+		Example 2:
+		Input: head = [1,2], pos = 0
+		Output: tail connects to node index 0
+		Explanation: There is a cycle in the linked list, 
+		where tail connects to the first node.
+	 */
+	// Time complexity -> O(n), Space complexity -> O(n)
+	public LinkedListNode detectCycle3(LinkedListNode head) {
+	    Set<LinkedListNode> nodeSet = new HashSet<>();
+
+	    LinkedListNode curr = head;
+	    while (curr != null) {
+	        if (nodeSet.contains(curr)) return curr;
+	        
+	        nodeSet.add(curr);
+	        curr = curr.next;
+	    }
+
+	    return null;
+	}
+	
+	// Time complexity -> O(n), Space complexity -> O(1)
+	public LinkedListNode detectCycle4(LinkedListNode head) {
+		LinkedListNode slow = head;
+		LinkedListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) break;
+        }
+
+        if (fast != null && fast.next != null) {
+            slow = head;
+
+            while (fast != slow) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            return fast;
+        }
+        
+        return null;
+    }
 	
     public static void main(String args[]) {
         int[][] input = { { 2, 4, 6, 8, 10, 12 }, { 1, 3, 5, 7, 9, 11 },
