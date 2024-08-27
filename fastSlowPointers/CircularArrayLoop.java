b418ff5ecba3693cb4998a50c78939881dacd6e8
Neeraj Singh <869neerajsingh@gmail.com>
2024-08-27 12:28:11 +0530

    feat: use better approach to solve circular array loop problem

--- fastSlowPointers/CircularArrayLoop.java
+++ fastSlowPointers/CircularArrayLoop.java
@@ -1,5 +1,6 @@
 package fastSlowPointers;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -51,7 +52,7 @@ public class CircularArrayLoop {
 	    }
 	  }
 	  
-   public static boolean circularArrayLoop(int[] nums) {
+   public static boolean circularArrayLoop1(int[] nums) {
       int n = nums.length;
       
       HashMap<Integer, Boolean> map = new HashMap<>();
@@ -103,5 +104,67 @@ public class CircularArrayLoop {
       
       return false;
    }
+   
+   // Time complexity -> O(n * n), Space complexity -> O(1)
+   public static boolean circularArrayLoop2(int[] nums) {
+	   int n = nums.length;
+	
+	   for (int i = 0; i < n; i++) {
+	   	int slow = i;
+	   	int fast = i;
+	   	boolean forward = nums[i] >= 0;
+	
+	   	while (true) {
+	   		slow = nextStep(nums[slow], slow, n);
+	   		if (notValidCycle(nums[slow], forward , n)) break;
+	
+	   		fast = nextStep(nums[fast], fast, n);
+	   		if (notValidCycle(nums[fast], forward , n)) break;
+	
+	   		fast = nextStep(nums[fast], fast, n);
+	   		if (notValidCycle(nums[fast], forward , n)) break;
+	
+	   		if (slow == fast) return true;
+	   	}
+	   }
+	
+	   return false;	
+   }
+
+   public static int nextStep(int step, int currIdx, int size) {
+	   	int nextStep = (currIdx + step) % size;
+	   	if (nextStep < 0) nextStep += size;
+	
+	   	return nextStep;
+   }
+
+   public static boolean notValidCycle(int currValue, boolean prevDirection, int size) {
+	   	boolean currDirection = currValue >= 0;
+	
+	   	if (currDirection != prevDirection || Math.abs(currValue % size) == 0) return true;
+	
+	   	return false;
+   }
+	   
+	// Driver code
+    public static void main(String[] args) {
+        int[][] input = {
+                {-2, -3, -9},
+                {-5, -4, -3, -2, -1},
+                {-1, -2, -3, -4, -5},
+                {2, 1, -1, -2},
+                {-1, -2, -3, -4, -5, 6},
+                {1, 2, -3, 3, 4, 7, 1},
+                {2, 2, 2, 7, 2, -1, 2, -1, -1}
+        };
+
+        for (int i = 0; i < input.length; i++) {
+            System.out.println((i + 1) + ".\tCircular array = " + Arrays.toString(input[i]) + "\n");
+            boolean result = circularArrayLoop2(input[i]);
+            System.out.println("\tFound Loop = " + result);
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+        }
+    }
+
 
 }
