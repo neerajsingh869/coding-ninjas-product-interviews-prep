dd1cfb30c74cc62fdc3ac4dafeb6b16d0a6d55fc
Neeraj Singh <869neerajsingh@gmail.com>
2024-08-28 16:43:22 +0530

    feat; solve palindrome problem check in linked list problem using fast and slow pointer

---
+++ fastSlowPointers/PalindromeLinkedList.java
@@ -0,0 +1,46 @@
+package fastSlowPointers;
+
+import GenericClasses.LinkedListNode;
+import GenericClasses.LinkedListReversal;
+
+public class PalindromeLinkedList {
+	
+	/*
+	 * Given the head of a linked list, your task is to check whether the linked list 
+	 * is a palindrome or not. Return TRUE if the linked list is a 
+	 * palindrome; otherwise, return FALSE.
+
+		Note: The input linked list prior to the checking process should be 
+		identical to the list after the checking process has been completed.
+	 */
+	
+	public static boolean palindrome(LinkedListNode head) {
+      if (head == null || head.next == null) return true;
+      
+      LinkedListNode fast = head, slow = head;
+      while (fast.next != null && fast.next.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+      }
+      
+      LinkedListNode secondHead = slow.next;
+      slow.next = null;
+      
+      secondHead = LinkedListReversal.reverseLinkedList(secondHead);
+      
+      boolean isPalindrome = true;
+      LinkedListNode firstLLTemp = head, secondLLTemp = secondHead;
+      while (firstLLTemp != null && secondLLTemp != null) {
+        if (firstLLTemp.data != secondLLTemp.data) {
+          isPalindrome = false;
+          break;
+        }
+        
+        firstLLTemp = firstLLTemp.next;
+        secondLLTemp = secondLLTemp.next;
+      }
+      
+      slow.next = LinkedListReversal.reverseLinkedList(secondLLTemp);
+      return isPalindrome;
+    }
+}
